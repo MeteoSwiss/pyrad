fd0cefa57643390db90d9952e3996534339eb704
Daniel Wolfensberger <daniel.wolfensberger@meteoswiss.ch>
2021-02-11 13:21:30 +0100

    changed version_updater

--- version_updater.sh
+++ version_updater.sh
@@ -1,5 +1,11 @@
 
 #!/bin/bash
+echo "This is a command line tool to help you release new pyart/pyrad version"
+echo "Please make sure you run this file from within the pyrad root folder (i.e. where this file is located)"
+echo "Type enter to continue"
+read continue
+
+dir=dirname "$0"
 echo Do you want to release a new pyART version y/n?
 read updatepyart
 if [[ $updatepyart == "y" ]]; then
@@ -11,21 +17,42 @@ read minor
 echo "What is the micro version?"
 read micro
 
+cd $dir/src/pyart/
+
 echo "Version number is $major.$minor.$micro"
 echo "Updating setup.py"
-sed -i "/MAJOR =/c\MAJOR = $major" ./src/pyart/setup.py
-sed -i "/MINOR =/c\MINOR = $minor" ./src/pyart/setup.py
-sed -i "/MICRO =/c\MICRO = $micro" ./src/pyart/setup.py
+sed -i "/MAJOR =/c\MAJOR = $major" setup.py
+sed -i "/MINOR =/c\MINOR = $minor" setup.py
+sed -i "/MICRO =/c\MICRO = $micro" setup.py
 
 echo "Updating /doc/source/conf.py"
-sed -i "/version = u/c\version = u'$major.$minor'" ./src/pyart/doc/source/conf.py
-sed -i "/release = u/c\release = u'$major.$minor.$micro'" ./src/pyart/doc/source/conf.py
+sed -i "/version = u/c\version = u'$major.$minor'" ./doc/source/conf.py
+sed -i "/release = u/c\release = u'$major.$minor.$micro'" ./doc/source/conf.p
 
-echo "Done! You can now create and push the new release with:"
-echo "cd ./src/pyart/" 
-echo "git tag v$major.$minor"
-echo "git push origin v$major.$minor."
+echo "Done!"
+echo "Do you want to push the new tag on github? y/n"
+read pushtag
+if [[ $pushtag == "y" ]]; then
+git tag "v${major}.${minor}"
+git push origin "v${major}.${minor}"
 fi
+
+echo "Do you want to create a new PyPI (pip) package? y/n"
+read releasepypi
+if [[ $releasepypi == "y" ]]; then
+python setup.py sdist
+echo "Should it be a test package or a final (official) package? test/final"
+read pkgtype
+if [[ $pkgtype == "test" ]]; then
+python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+fi
+if [[ $pkgtype == "final" ]]; then
+python -m twine upload dist/*
+fi
+fi
+
+fi
+
 
 echo Do you want to release a new pyRAD version y/n?
 read updatepyrad
@@ -38,6 +65,8 @@ read minor
 echo "What is the micro version?"
 read micro
 
+cd $dir/src/pyrad_proc/
+
 echo "Version number is $major.$minor.$micro"
 echo "Updating setup.py"
 sed -i "/MAJOR =/c\MAJOR = $major" ./src/pyrad_proc/setup.py
@@ -48,7 +77,29 @@ echo "Updating /doc/source/conf.py"
 sed -i "/version = u/c\version = u'$major.$minor'" ./doc/source/conf.py
 sed -i "/release = u/c\release = u'$major.$minor.$micro'" ./doc/source/conf.py
 
-echo "Done! You can now create and push the new release with:"
-echo "git tag v$major.$minor"
-echo "git push origin v$major.$minor."
+
+echo "Done!"
+echo "Do you want to push the new tag on github? y/n"
+read pushtag
+if [[ $pushtag == "y" ]]; then
+git tag "v${major}.${minor}"
+git push origin "v${major}.${minor}"
+fi
+
+echo "Do you want to create a new PyPI (pip) package? y/n"
+read releasepypi
+if [[ $releasepypi == "y" ]]; then
+python setup.py sdist
+echo "Should it be a test package or a final (official) package? test/final"
+read pkgtype
+if [[ $pkgtype == "test" ]]; then
+python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+fi
+if [[ $pkgtype == "final" ]]; then
+python -m twine upload dist/*
+fi
+fi
+
+fi
+
 fi
