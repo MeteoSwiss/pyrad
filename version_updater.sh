3c1266db7858e0035e294600703fbe81a95dad2a
Daniel Wolfensberger <daniel.wolfensberger@meteoswiss.ch>
2021-02-11 14:20:57 +0100

    Changed version numbers

--- version_updater.sh
+++ version_updater.sh
@@ -1,11 +1,11 @@
 
 #!/bin/bash
 echo "This is a command line tool to help you release new pyart/pyrad version"
-echo "Please make sure you run this file from within the pyrad root folder (i.e. where this file is located)"
+echo "Please confirm that you are on master branch and you have added and commit all modifications you want to include in the tag in the master branch!"
 echo "Type enter to continue"
 read continue
 
-dir=dirname "$0"
+dir=$(dirname "$0")
 echo Do you want to release a new pyART version y/n?
 read updatepyart
 if [[ $updatepyart == "y" ]]; then
@@ -17,25 +17,31 @@ read minor
 echo "What is the micro version?"
 read micro
 
-cd $dir/src/pyart/
+cd $dir
 
 echo "Version number is $major.$minor.$micro"
 echo "Updating setup.py"
-sed -i "/MAJOR =/c\MAJOR = $major" setup.py
-sed -i "/MINOR =/c\MINOR = $minor" setup.py
-sed -i "/MICRO =/c\MICRO = $micro" setup.py
+sed -i "/MAJOR =/c\MAJOR = $major" $dir/src/pyrad_proc/setup.py
+sed -i "/MINOR =/c\MINOR = $minor" $dir/src/pyrad_proc/setup.py
+sed -i "/MICRO =/c\MICRO = $micro" $dir/src/pyrad_proc/setup.py
 
 echo "Updating /doc/source/conf.py"
-sed -i "/version = u/c\version = u'$major.$minor'" ./doc/source/conf.py
-sed -i "/release = u/c\release = u'$major.$minor.$micro'" ./doc/source/conf.p
+sed -i "/version = u/c\version = u'$major.$minor'" $dir/doc/source/conf.py
+sed -i "/release = u/c\release = u'$major.$minor.$micro'" $dir/doc/source/conf.py
 
 echo "Done!"
+echo "We will now push the version number updated on github"
+git add --all
+git commit -m "Changed version numbers"
+git push origin master
+
 echo "Do you want to push the new tag on github? y/n"
 read pushtag
 if [[ $pushtag == "y" ]]; then
 git tag "v${major}.${minor}"
 git push origin "v${major}.${minor}"
 fi
+echo "Done!"
 
 echo "Do you want to create a new PyPI (pip) package? y/n"
 read releasepypi
@@ -50,7 +56,7 @@ if [[ $pkgtype == "final" ]]; then
 python -m twine upload dist/*
 fi
 fi
-
+echo "Done!"
 fi
 
 
@@ -65,18 +71,22 @@ read minor
 echo "What is the micro version?"
 read micro
 
-cd $dir/src/pyrad_proc/
+cd $dir/
 
 echo "Version number is $major.$minor.$micro"
 echo "Updating setup.py"
-sed -i "/MAJOR =/c\MAJOR = $major" ./src/pyrad_proc/setup.py
-sed -i "/MINOR =/c\MINOR = $minor" ./src/pyrad_proc/setup.py
-sed -i "/MICRO =/c\MICRO = $micro" ./src/pyrad_proc/setup.py
+sed -i "/MAJOR =/c\MAJOR = $major" $dir/src/pyrad_proc/setup.py
+sed -i "/MINOR =/c\MINOR = $minor" $dir/src/pyrad_proc/setup.py
+sed -i "/MICRO =/c\MICRO = $micro" $dir/src/pyrad_proc/setup.py
 
 echo "Updating /doc/source/conf.py"
 sed -i "/version = u/c\version = u'$major.$minor'" ./doc/source/conf.py
 sed -i "/release = u/c\release = u'$major.$minor.$micro'" ./doc/source/conf.py
 
+echo "We will now push the version number updated on github"
+git add --all
+git commit -m "Changed version numbers"
+git push origin master
 
 echo "Done!"
 echo "Do you want to push the new tag on github? y/n"
