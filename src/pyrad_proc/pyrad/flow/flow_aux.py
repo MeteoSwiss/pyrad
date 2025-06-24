417a7c65d5f4d94a7a2842e84bdc522d0d02922f
Daniel Wolfensberger <daniel.wolfensberger@meteoswiss.ch>
2025-06-24 07:17:42 +0000

    FIX: missing import of DEFAULT_CONFIG

--- src/pyrad_proc/pyrad/flow/flow_aux.py
+++ src/pyrad_proc/pyrad/flow/flow_aux.py
@@ -56,7 +56,8 @@ except ImportError:
     _MPROFILE_AVAILABLE = False
 
 from pyrad import proc
-from ..io.config import read_config, DEFAULT_CONFIG
+from ..io.config import read_config
+from ..io.default_config import DEFAULT_CONFIG
 from ..io.read_data_radar import get_data
 from ..io.write_data import write_to_s3
 from ..io.io_aux import get_datetime, get_file_list, get_scan_list
